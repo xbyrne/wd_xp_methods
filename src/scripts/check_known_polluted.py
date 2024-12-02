6637465d89fd6d0677512a07f55b81c53b118949
Xander Byrne <ajnb3@cam.ac.uk>
2024-12-02 13:13:36 +0000

    Making check_known_polluted.py enormously more efficient

--- src/scripts/check_known_polluted.py
+++ src/scripts/check_known_polluted.py
@@ -16,23 +16,26 @@ import pandas as pd
 from tqdm import tqdm
 
 # --------------------------
-# Loading external datasets, and selecting the objects which have XP spectra (`ids`)
+# Loading external datasets
 # GF21xSDSS
 gf21sdss = pd.read_csv("../data/external/gf21_sdss.csv")  # 41820 rows, from VizieR
 gf21sdss.drop_duplicates(subset="GaiaEDR3", inplace=True)  # 41820 -> 32169
 gf21sdss.set_index("GaiaEDR3", inplace=True)
+gf21sdss_index = set(gf21sdss.index.values)  # For faster checking
 
 # MWDD
 mwdd = pd.read_csv("../data/external/mwdd.csv")  # 70698 rows, from MWDD 2024-11-06
 mwdd.drop_duplicates(subset="gaiaedr3", inplace=True)  # 70698 -> 61198
 mwdd.dropna(inplace=True)  # 61198 -> 56745
 mwdd.set_index("gaiaedr3", inplace=True)
+mwdd_index = set(mwdd.index.values)  # For faster checking
 
 # PEWDD
 pewdd = pd.read_csv("../data/external/pewdd.csv")  # 3546 rows, from Github 2024-11-06
 pewdd.set_index("Gaia_designation", inplace=True)
 pewdd = pewdd[pewdd.index.fillna("").str.startswith("Gaia DR3")]  # 3546 -> 2979
 pewdd.index = pewdd.index.str.replace("Gaia DR3 ", "").astype(int)
+pewdd_index = set(pewdd.index.values)  # For faster checking
 
 
 # --------------------------
@@ -47,14 +50,20 @@ def check_whether_obj_polluted(id_):
     - (-1, -1, -1) -> -1  (None of the datasets are conclusive)
     - (1, 0, -1) -> 1  (A dataset saying it's polluted supercedes one saying it's not)
     """
-    gf21sdss_polluted = check_gf21sdss(id_)
-    mwdd_polluted = check_mwdd(id_)
-    pewdd_polluted = check_pewdd(id_)
-    checklist = np.array([gf21sdss_polluted, mwdd_polluted, pewdd_polluted])
-
     # If any of the datasets classify the object as polluted, return 1
-    if 1 in checklist:
+    # Check them in order so we can return early if a dataset classes the object as polluted
+    mwdd_polluted = check_mwdd(id_)
+    if mwdd_polluted == 1:
         return 1
+    gf21sdss_polluted = check_gf21sdss(id_)
+    if gf21sdss_polluted == 1:
+        return 1
+    pewdd_polluted = check_pewdd(id_)
+    if pewdd_polluted == 1:
+        return 1
+
+    # If we get to this stage, none of the datasets classify the object as polluted
+    checklist = np.array([gf21sdss_polluted, mwdd_polluted, pewdd_polluted])
     # If any of the datasets classify the object as non-polluted, but
     # none of them classify it as polluted, return 0
     if 0 in checklist:
@@ -72,7 +81,7 @@ def check_gf21sdss(id_):
             or ends in "Z:"
     - False: otherwise
     """
-    if id_ in gf21sdss.index:
+    if id_ in gf21sdss_index:
         cl = gf21sdss.loc[id_, "specClass"]
         if "Z" in cl and not cl.endswith("Z:"):  # Polluted
             return 1
@@ -89,11 +98,11 @@ def check_mwdd(id_):
     Checks whether a WD with a given Gaia EDR3 ID is polluted according to the MWDD dataset.
     Returns:
     - True: if the object is classified as D*Z* in the dataset
-    - None: if the object isn't in the dataset
-    - False: otherwise
+    - None: if the object is classified as CND, D, or '?', or isn't in the dataset
+    - False: classified as something else
     """
     disqualifying_substrings = ["Z?", "Z:", "+", "/"]
-    if id_ in mwdd.index:
+    if id_ in mwdd_index:
         cl = mwdd.loc[id_, "spectype"]
         if "Z" in cl and not any(
             ss in cl for ss in disqualifying_substrings
@@ -114,7 +123,7 @@ def check_pewdd(id_):
     - True: if the object is in the dataset
     - None: otherwise
     """
-    if id_ in pewdd.index:  # Polluted
+    if id_ in pewdd_index:  # Polluted
         return 1
     # else
     return -1  # Not sure - not in PEWDD
@@ -126,9 +135,11 @@ if __name__ == "__main__":
     ids = fl["ids"]  # 107164 Gaia EDR3 IDs
 
     ispolluted = pd.DataFrame(index=ids.astype(str), columns=["is_polluted"])
+    isps = np.zeros_like(ids, dtype=int)
 
-    for idd in tqdm(ids):
+    for i, idd in tqdm(enumerate(ids)):
         ISP = check_whether_obj_polluted(idd)
-        ispolluted.loc[idd, "is_polluted"] = ISP
+        isps[i] = ISP
 
+    ispolluted["is_polluted"] = isps
     ispolluted.to_csv("../data/interim/is_polluted.csv", index_label="gaiaedr3")
