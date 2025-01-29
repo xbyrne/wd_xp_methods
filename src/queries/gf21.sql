065f8f50b898e01d5804aa57c76a838b0d94b96a
Xander Byrne <ajnb3@cam.ac.uk>
2025-01-29 10:57:13 +0000

    Sourcing more data on the WDs. Might cut back again later to reduce file sizes downloaded

--- src/queries/gf21.sql
+++ src/queries/gf21.sql
@@ -3,9 +3,9 @@
 -- We used TOPCAT to post the query and download the results
 -- The resulting table should be saved to `src/data/interim/gf21_filtered.csv`
 
-SELECT GaiaEDR3, "Gmag", BPmag, RPmag
+SELECT GaiaEDR3, "Gmag", BPmag, RPmag, RA_ICRS, DE_ICRS, Plx, Pwd, TeffH, SDSS12
 FROM "J/MNRAS/508/3877/maincat"
 WHERE
     o_BPmag >= 10 AND -- phot_bp_n_obs (Andrae+23)
     o_RPmag >= 15 AND -- phot_rp_n_obs (")
-    Nper >= 10        -- visibility_periods_used (Lindegren+18)g
+    Nper >= 10        -- visibility_periods_used (Lindegren+18)
